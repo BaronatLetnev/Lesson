3af80a9b42068b31f7f15cad504ba1d93d58ea9d
BaronatLetnev <buro.kika@gmail.com>
2024-11-10 20:47:01 +0400

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,12 @@
+# 1 program
+lesson_count = 12
+hour_count = 1.5
+name="Python,"
+print ("Курс:",name,"всего заданий:",lesson_count,",","затрачено часов:", hour_count,",","время на 1 задание:",hour_count/lesson_count,"часа")
+# 2 program
+first_name = 'Вася'
+apple_count = 10
+second_name = 'Пете'
+orange_count = 15
+print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+print("У Васи всего",apple_count+orange_count,"фруктов") # пофантазировал
