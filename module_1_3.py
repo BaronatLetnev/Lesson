65b8f9bc46a70dcf327b9e683ed7c2895eeaed4b
BaronatLetnev <buro.kika@gmail.com>
2024-11-10 20:23:03 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Vladimir"
+print ("Name:",name,type(name))
+age = 43
+print ("Age:",age,type(age))
+age = 45
+print ("New Age:",age,type(age))
+name = "is Student:"
+print (name,name==name)
+
